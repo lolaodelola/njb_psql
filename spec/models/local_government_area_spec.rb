e913b370383d4782b151b3ea02c38c9b784de153
lolaodelola <damzcodes@gmail.com>
2020-12-08 08:32:54 +0000

    lga -suite -failing

--- spec/models/local_government_area_spec.rb
+++ spec/models/local_government_area_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe LocalGovernmentArea, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'model set up' do
+    # describe 'columns' do
+    #   it { should have_db_column(:name) }
+    # end
+    #
+    # describe 'validations' do
+    #   it { should validate_presence_of(:name) }
+    # end
+    #
+    # describe 'associations' do
+    #   it { should belongs_to(:city) }
+    #   it { should have_many(:end_users) }
+    #   it { should have_many(:jobs) }
+    # end
+  end
 end
