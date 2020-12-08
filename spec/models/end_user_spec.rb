377aadb628672603c2707dc0157373a23169c079
lolaodelola <damzcodes@gmail.com>
2020-12-08 07:43:21 +0000

    failing associations tests

--- spec/models/end_user_spec.rb
+++ spec/models/end_user_spec.rb
@@ -8,5 +8,12 @@ RSpec.describe EndUser, type: :model do
       it { should validate_presence_of(:phone_number) }
       it { should validate_presence_of(:twilio_uid) }
     end
+
+    describe 'associations' do
+      it { should belong_to(:city) }
+      # it { should belong_to(:local_government_area) }
+      # it { should have_many(:sent_jobs) }
+      # it { should have_many(:industries) }
+    end
   end
 end
