287279f71743f30f0f2a596b2acce5120493c06d
lolaodelola <damzcodes@gmail.com>
2020-12-08 08:53:18 +0000

    title -job -column -passing

--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Job, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'model set up' do
+    describe 'columns' do
+      it { should have_db_column(:title) }
+    end
+    #
+    # describe 'validations' do
+    #   it { should validate_presence_of(:title) }
+    # end
+    #
+    # describe 'associations' do
+    #   it { should belong_to(:end_user).with_foreign_key('employer_id') }
+    #   it { should belong_to(:local_governement_area).with_foreign_key('lga_id') }
+    #   it { should belong_to(:city) }
+    #   it { should belong_to(:industry) }
+    #   it { should have_many(:sent_jobs) }
+    # end
+  end
 end
