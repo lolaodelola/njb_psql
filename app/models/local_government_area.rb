d7ae1891fa439d58fc970e7d7a8b88e221a77779
lolaodelola <damzcodes@gmail.com>
2020-12-08 08:34:21 +0000

    name -lga -validation -passing

--- app/models/local_government_area.rb
+++ app/models/local_government_area.rb
@@ -1,2 +1,3 @@
 class LocalGovernmentArea < ApplicationRecord
+  validates_presence_of :name
 end
--- spec/models/local_government_area_spec.rb
+++ spec/models/local_government_area_spec.rb
@@ -5,10 +5,10 @@ RSpec.describe LocalGovernmentArea, type: :model do
     describe 'columns' do
       it { should have_db_column(:name) }
     end
-    #
-    # describe 'validations' do
-    #   it { should validate_presence_of(:name) }
-    # end
+
+    describe 'validations' do
+      it { should validate_presence_of(:name) }
+    end
     #
     # describe 'associations' do
     #   it { should belongs_to(:city) }
