ee8b3cd8534379522d6cca404c9b23e8ca6cfa37
lolaodelola <damzcodes@gmail.com>
2020-12-09 07:54:39 +0000

    city -job -association -passing

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,4 +3,5 @@ class Job < ApplicationRecord
   validates_presence_of :title
   belongs_to :end_user, foreign_key: 'employer_id'
   belongs_to :local_government_area, foreign_key: 'lga_id'
+  belongs_to :city
 end
