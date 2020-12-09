4ccc3f8772e262165d2031ce9fa82981e2d6e924
lolaodelola <damzcodes@gmail.com>
2020-12-09 08:30:46 +0000

    sent_at -sent_jobs -column -passing

--- spec/models/sent_job_spec.rb
+++ spec/models/sent_job_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe SentJob, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'model set up' do
+    describe 'columns' do
+      it { should have_db_column(:sent_at) }
+    end
+    #
+    # describe 'validations' do
+    #   it { should validate_presence_of(:sent_at) }
+    # end
+    #
+    # describe 'associations' do
+    #   it { should belong_to(:end_user) }
+    #   it { should belong_to(:jobs) }
+    # end
+  end
 end
