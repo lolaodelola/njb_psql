5ca57a23be0de9be0edc40ae6bdf0567cb66c391
lolaodelola <damzcodes@gmail.com>
2020-12-09 08:09:06 +0000

    eu_industries -industry -association -passing

--- app/models/industry.rb
+++ app/models/industry.rb
@@ -1,3 +1,4 @@
 class Industry < ApplicationRecord
   validates_presence_of :name
+  has_many :end_user_industries
 end
--- spec/models/industry_spec.rb
+++ spec/models/industry_spec.rb
@@ -9,11 +9,11 @@ RSpec.describe Industry, type: :model do
     describe 'validations' do
       it { should validate_presence_of(:name) }
     end
-    #
-    # describe 'associations' do
-    #   it { should have_many(:end_user_industries) }
-    #   it { should have_many(:end_users).through(:end_user_industries) }
-    #   it { should have_many(:jobs) }
-    # end
+
+    describe 'associations' do
+      it { should have_many(:end_user_industries) }
+      # it { should have_many(:end_users).through(:end_user_industries) }
+      # it { should have_many(:jobs) }
+    end
   end
 end
